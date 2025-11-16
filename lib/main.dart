6e5090fac4ec9d4f6a3b803cf39c387567142926
RJBarredo <bleedingkey@gmail.com>
2025-11-16 17:23:00 +0800

    Fixed some bugs and added logo. Also Added some functions such as creating/adding subject in the home page

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class TuonApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tuon',
+      title: 'Tuon.',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
